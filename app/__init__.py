ff3437840e7c9ddb0725c64f5872267917e9395e
Benson <bensongathu23@gmail.com>
2021-06-12 22:08:05 +0300

    User model added

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,26 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
-
-#Initializing our application
-app = Flask(__name__)
-
-#Setting up configuration
-app.config.from_object(DevConfig)
-
-#Initializing flask extension
-bootstrap = Bootstrap(app)
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 
-from app import views
-from app import error
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    #Initializing our application
+    app = Flask(__name__)
+
+    #Setting up configuration
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extension
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    #Registering the blueprint
+    from. main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    return app
--- app/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-
-    pass
-
-class ProdConfig(Config):
-    pass
-
-class DevConfig(Config):
-    DEBUG = True
