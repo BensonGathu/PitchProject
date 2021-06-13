aa0eb0c157acdee8fac0064f9d9b3fafb6c06213
Benson <bensongathu23@gmail.com>
2021-06-13 15:53:36 +0300

    templates added

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required,Length,DataRequired
+from flask_wtf.file import FileField,file_allowed
+
+class UpdateProfileForm(FlaskForm):
+    bio = TextAreaField("More about you",validators=[Required()])
+    submit = SubmitField("Submit")
+
+class PitchUploadForm(FlaskForm):
+    category = SelectField('Choose category: ',validators=[DataRequired()],choices=[('Interview Pitch','Interview Pitch'),('Products Pitch','Products Pitch'),('IT World Pitch','IT World Pitch'),('Political Pitch','Political Pitch'),('Pickup Lines','Pickup Lines')])
+    pitch = TextAreaField("Your Pitch",validators=[DataRequired()])
+    submit = SubmitField("Post Pitch")
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField("Comment",validators=[DataRequired()])
+    submit = SubmitField("Post Comment")
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,17 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
+from .forms import UpdateProfileForm,PitchUploadForm,CommentsForm
+from .. import db
 
 #Views
-@main.route("/")
+@main.route("/",methods = ['GET','POST'])
 def index():
     title = "Lets get Pitched"
     return render_template("index.html",title=title)
+
+@main.route("/pitch")
+def pitch():
+    pitches = Pitch.query.filter_by().first()
+
+    return render_template("pitch.html",pitches=pitches) 
