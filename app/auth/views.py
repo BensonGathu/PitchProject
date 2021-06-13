415cd47116094c779742ff9251be10f57f4ca909
Benson <bensongathu23@gmail.com>
2021-06-13 18:22:22 +0300

    update info bio added

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from . import auth
+from flask_login import login_user,logout_user,login_required
 
 
 @auth.route("/register",methods=["GET","POST"])
@@ -30,3 +31,10 @@ def login():
 
     title = "Pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
+from .. models import Pitch,Comment,User,Votes,Category
 from . import main
 from flask_login import login_required
 from .forms import UpdateProfileForm,PitchUploadForm,CommentsForm
@@ -15,3 +16,31 @@ def pitch():
     pitches = Pitch.query.filter_by().first()
 
     return render_template("pitch.html",pitches=pitches) 
+
+@main.route("/user/<uname>")
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfileForm()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
