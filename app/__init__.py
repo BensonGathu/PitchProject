7f13f2c964a4aa32707d2dbf6bd99c1f26468420
Benson <bensongathu23@gmail.com>
2021-06-14 08:03:24 +0300

    email added

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,26 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "auth.login"
 
+
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
     #Initializing our application
     app = Flask(__name__)
+
+    #configure UploadSet
+    #configure_uploads(app,photos)
 
     #Setting up configuration
     app.config.from_object(config_options[config_name])
@@ -24,6 +31,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     #Registering the blueprint
     from. main import main as main_blueprint
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 export SECRET_KEY="vcxz4321"
 
+export MAIL_USERNAME="unknownknowns23@gmail.com"
+export MAIL_PASSWORD="vcxz4321qwerty"
+
 python3 manage.py server
